22d207f92ad8f01bf8d2730cbdd830d36a55e867
Botnev Artem <arbotnev@yandex.ru>
2018-04-12 15:35:35 +0300

    task2 solved with annotation

--- src/io/khasang/reflection/Main.java
+++ src/io/khasang/reflection/Main.java
@@ -4,9 +4,9 @@ import io.khasang.reflection.di.Context;
 
 public class Main {
     public static void main(String[] args) {
-//        Context<Car> context = new Context<>("config.xml");
-//        Context<Car> context = new Context<>("random_config.xml");
-        Context<Car> context = new Context<>("random_config_annotation.xml");
+//        Context<Car> context = new Context<>("config.xml"); //first task
+//        Context<Car> context = new Context<>("random_config.xml"); //second task without annotation
+        Context<Car> context = new Context<>("random_config_annotation.xml"); // second task with annotation
         Car car = context.getBean("car"); // DZ generics
 
         System.out.println(car);
